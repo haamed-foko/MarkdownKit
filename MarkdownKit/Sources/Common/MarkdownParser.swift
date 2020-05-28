ca518f0679fb0d8ed0e0a3fd612b68098620f6fd
Haamed Sultani <haamed@fokoretail.com>
2020-05-28 15:44:54 -0400

    Remove unused Markdown Elements from parsing list

--- MarkdownKit/Sources/Common/MarkdownParser.swift
+++ MarkdownKit/Sources/Common/MarkdownParser.swift
@@ -137,10 +137,8 @@ open class MarkdownParser {
                                   range: NSRange(location: 0, length: attributedString.length))
     attributedString.addAttribute(.foregroundColor, value: color,
                                   range: NSRange(location: 0, length: attributedString.length))
-    var elements: [MarkdownElement] = escapingElements
-    elements.append(contentsOf: defaultElements)
+    var elements: [MarkdownElement] = defaultElements
     elements.append(contentsOf: customElements)
-    elements.append(contentsOf: unescapingElements)
     elements.forEach { element in
       element.parse(attributedString)
     }
